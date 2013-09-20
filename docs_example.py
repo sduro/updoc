762372ca00ae2d32f9e634e4116d7e82c6d97f8f
Sergi <sergiduro@gmail.com>
2013-09-20 14:56:37 +0200

    Update docs_example.py

--- docs_example.py
+++ docs_example.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#source: https://code.google.com/p/gdata-python-client/source/browse/samples/docs/docs_example.py
 
 __author__ = ('api.jfisher (Jeff Fisher), '
               'e.bidelman (Eric Bidelman)')
